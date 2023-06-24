6649e2baa501f1ec709bdaaa453c55e65b5726a4
JuanCarvajalD <125924849+JuanCarvajalD@users.noreply.github.com>
2023-06-23 19:18:54 -0500

    Codigo Data-ingestion--cluster

--- pregunta.py
+++ pregunta.py
@@ -1,21 +1,25 @@
-"""
-Ingestión de datos - Reporte de clusteres
------------------------------------------------------------------------------------------
-
-Construya un dataframe de Pandas a partir del archivo 'clusters_report.txt', teniendo en
-cuenta que los nombres de las columnas deben ser en minusculas, reemplazando los espacios
-por guiones bajos; y que las palabras clave deben estar separadas por coma y con un solo 
-espacio entre palabra y palabra.
-
-
-"""
 import pandas as pd
-
+import numpy as np
+import re
 
 def ingest_data():
+    df_temp = pd.read_fwf("clusters_report.txt", skiprows=4, colspecs='infer', names=['1', '2', '3', '4'])
+    df = pd.DataFrame(columns=['cluster', 'cantidad_de_palabras_clave', 'porcentaje_de_palabras_clave', 'principales_palabras_clave'])
 
-    #
-    # Inserte su código aquí
-    #
+    for indice in range(len(df_temp)):
+        if not np.isnan(df_temp.iloc[indice]['1']):
+            df.loc[len(df.index)] = [df_temp.iloc[indice]['1'], df_temp.iloc[indice]['2'], df_temp.iloc[indice]['3'], df_temp.iloc[indice]['4']]
+        else:
+            df.at[len(df.index)-1, 'principales_palabras_clave'] += " " + df_temp.iloc[indice]['4']
+
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: re.sub('\s+', ' ', x))
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: re.sub(',\s*', ', ', x))
+    df['principales_palabras_clave'] = df['principales_palabras_clave'].apply(lambda x: x.rstrip('.'))
+
+    df['cluster'] = df['cluster'].apply(int)
+    df['cantidad_de_palabras_clave'] = df['cantidad_de_palabras_clave'].apply(int)
+
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].apply(lambda x: float(re.sub(',', '.', re.findall('\d+,\d+', x)[0])))
 
     return df
+
